0cbbaa226b07701ec84e8ec45ba22076ef0495e1
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-06 20:57:15 -0400

    added import to workbox-webpack-plugin destructure

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, WorkboxPlugin } = require('workbox-webpack-plugin');
 
 // DONE: Add and configure workbox plugins for a service worker and manifest file.
 // DONE: Add CSS loaders and babel to webpack.
